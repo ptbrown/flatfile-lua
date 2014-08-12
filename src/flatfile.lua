59a20582dc3da69d15a5a8611e8fa7b7122e5861
Patrick Brown <ptbrown@whoopdedo.org>
2014-08-12 02:27:40 -0400

    Add function doc comments

--- src/flatfile.lua
+++ src/flatfile.lua
@@ -19,12 +19,11 @@ local flatfile = {}
 local reader = {}
 local writer = {}
 
---[[ Open a file to read or write table data from fixed-length records.
-
-    @arg    source  string|file
-    @arg    mode    string
-    @return object
-  ]]
+--- Open a file to read or write table data from fixed-length records.
+--  
+--  @param  source  string|file
+--  @param  mode    string
+--  @return object
 function flatfile.open(source, mode)
     if mode then
         mode = string.sub(mode, 1, 1)
@@ -61,15 +60,38 @@ function flatfile.open(source, mode)
     return setmetatable({source=source, mode=mode, columns={}}, meta)
 end
 
+--- Define the names of columns.
+--  
+--  1. A list of numbers: start1, end1, start2, end2, ...
+--  2. A list of names: field1, field2, ...
+--  3. A list of names and numbers: field1, start1, end1, ...
 local function definecolumns(self, ...)
 end
 
 reader.columns = definecolumns
 writer.columns = definecolumns
 
+--- Read the header lines.
+--  
+--  @param  skip        number (optional)
+--  @param  columnname  string (optional)
+--  @return string...
 function reader:header(skip, columnname)
+    if columnname == nil and type(skip) == 'string' then
+        columnname,skip = skip,0
+    elseif skip == nil then
+        skip = 0
+    end
+    local skipped = {}
+    for i = 1,skip do
+        skipped[i] = self.source:read()
+    end
 end
 
+--- Write a header.
+--  
+--  @param  string...
+--  @return number
 function writer:header(...)
     if self.mode == 'a' then
         return
@@ -77,25 +99,45 @@ function writer:header(...)
     return writelines(self.source, 0, ...)
 end
 
+--- Read one line as a list of values.
+--  
+--  @return string...
 local function readrowexpand(self)
 end
 
-local function readrowtable(self)
+--- Read one line as a table.
+--  
+--  @param  dest    table (optional)
+--  @return table
+local function readrowtable(self, dest)
+    dest = dest or {}
+    return dest
 end
 
+local function _readrowtable(self)
+    return readrowtable(self)
+end
+
+--- Iterator interface to reading lines from the file.
+--  
+--  @param  expand  boolean (optional)
+--  @return function|object
 function reader:rows(expand)
     if expand then
         return readrowexpand, self
     else
-        return readrowtable, self
+        return _readrowtable, self
     end
 end
 
+--- Read one or more lines from the file.
+--  
+--  @param  what    string (optional)
+--  @param  expand  boolean (optional)
+--  @return table|string...
 function reader:read(what, expand)
-    if expand == nil then
-        if type(what) == 'boolean' then
-            what,expand = 'r', what
-        end
+    if expand == nil and type(what) == 'boolean' then
+        expand,what = what, 'r'
     end
     what = what and strsub(what, 1, 1) or 'r'
     if what == 'r' then
@@ -120,9 +162,27 @@ function reader:read(what, expand)
     end
 end
 
+--- Read a line and store the results in a table.
+--  
+--  @param  destination table|function
+--  @param  expand      boolean (optional)
+--  @return table|...
 function reader:readinto(destination, expand)
+    if type(destination) == 'function' then
+        if expand then
+            return destination(assert(readrowexpand(self)))
+        else
+            return destination(assert(readrowtable(self)))
+        end
+    else
+        return readrowtable(self, destination)
+    end
 end
 
+--- Write a line to the file.
+--  
+--  @param  string...
+--  @return number
 function writer:write(...)
 end
 
